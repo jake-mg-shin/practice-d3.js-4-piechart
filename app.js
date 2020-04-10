8393a61d0a4dc2c9c98ca8cbe89c2b7eaa717d97
jake-mg-shin <60331338+jake-mg-shin@users.noreply.github.com>
2020-04-10 14:22:13 -0600

    finished inner

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const width = 600;
-const height = 600;
-const minYear = d3.min(birthData, (d) => d.year);
-const maxYear = d3.max(birthData, (d) => d.year);
-const orderedMonths = [
+var width = 600;
+var height = 600;
+var minYear = d3.min(birthData, (d) => d.year);
+var maxYear = d3.max(birthData, (d) => d.year);
+var orderedMonths = [
 	'January',
 	'February',
 	'March',
@@ -16,7 +16,7 @@ const orderedMonths = [
 	'November',
 	'December',
 ];
-const colors = [
+var colors = [
 	'#aec7e8',
 	'#a7cfc9',
 	'#9fd7a9',
@@ -31,15 +31,19 @@ const colors = [
 	'#c9c3c3',
 ];
 
-const quarterColors = ['#1f77b4', '#2ca02c', '#d62728', '#ff7f0e'];
+var quarterColors = ['#1f77b4', '#2ca02c', '#d62728', '#ff7f0e'];
 
-const colorScale = d3.scaleOrdinal().domain(orderedMonths).range(colors);
+var colorScale = d3.scaleOrdinal().domain(orderedMonths).range(colors);
 
-const svg = d3.select('svg').attr('width', width).attr('height', height);
+var svg = d3.select('svg').attr('width', width).attr('height', height);
 
 svg.append('g')
-	.attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
+	.attr('transform', 'translate(' + width / 2 + ', ' + height / 2 + ')')
 	.classed('chart', true);
+
+svg.append('g')
+	.attr('transform', 'translate(' + width / 2 + ', ' + height / 2 + ')')
+	.classed('inner-chart', true);
 
 svg.append('text')
 	.classed('title', true)
@@ -50,9 +54,15 @@ svg.append('text')
 
 drawGraph(minYear);
 
+d3.select('input')
+	.property('min', minYear)
+	.property('max', maxYear)
+	.property('value', minYear)
+	.on('input', () => drawGraph(+d3.event.target.value));
+
 function drawGraph(year) {
-	const yearData = birthData.filter((d) => d.year === year);
-	const arcs = d3
+	var yearData = birthData.filter((d) => d.year === year);
+	var arcs = d3
 		.pie()
 		.value((d) => d.births)
 		.sort(
@@ -60,12 +70,27 @@ function drawGraph(year) {
 				orderedMonths.indexOf(a.month) - orderedMonths.indexOf(b.month)
 		);
 
-	const path = d3
+	var innerArcs = d3
+		.pie()
+		.value((d) => d.births)
+		.sort((a, b) => a.quarter - b.quarter);
+
+	var path = d3
 		.arc()
 		.innerRadius(width / 4)
 		.outerRadius(width / 2 - 40);
 
-	const outer = d3.select('.chart').selectAll('.arc').data(arcs(yearData));
+	var innerPath = d3
+		.arc()
+		.innerRadius(0)
+		.outerRadius(width / 4);
+
+	var outer = d3.select('.chart').selectAll('.arc').data(arcs(yearData));
+
+	var inner = d3
+		.select('.inner-chart')
+		.selectAll('.arc')
+		.data(innerArcs(getDataByQuarter(yearData)));
 
 	outer
 		.enter()
@@ -75,5 +100,23 @@ function drawGraph(year) {
 		.merge(outer)
 		.attr('d', path);
 
+	inner
+		.enter()
+		.append('path')
+		.classed('arc', true)
+		.attr('fill', (d, i) => quarterColors[i])
+		.merge(inner)
+		.attr('d', innerPath);
+
 	d3.select('.title').text('Births by months and quarter for ' + year);
+}
+
+function getDataByQuarter(data) {
+	var quarterTallies = [0, 1, 2, 3].map((n) => ({ quarter: n, births: 0 }));
+	for (var i = 0; i < data.length; i++) {
+		var row = data[i];
+		var quarter = Math.floor(orderedMonths.indexOf(row.month) / 3);
+		quarterTallies[quarter].births += row.births;
+	}
+	return quarterTallies;
 }
